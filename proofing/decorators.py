db3055f21af74ab071f334dfbd1774848b7f6c82
Spencer Herzberg <spencer.herzberg@gmail.com>
2011-02-03 01:05:13 -0500

    added some docs for the sweet decorator

--- proofing/decorators.py
+++ proofing/decorators.py
@@ -4,6 +4,12 @@ from django.http import Http404
 
 from models import *
 
+'''
+This decorator is to check object permissions based on the rules defined for Gallery and Photo models (
+or really an model object that implements a is_users(user) method. The nice thing about this method is that
+instead of passing a string slug to a view, the actual object is passed (if the tests are passed and an
+object was found with get_object_or_404.
+'''
 
 # this could be better
 def check_perm_for(cls):
