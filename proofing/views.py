5833de4d6294dd7fa99e12dc36677a4320186c56
Spencer Herzberg <spencer.herzberg@gmail.com>
2011-02-03 23:16:06 -0500

    updated views to be more generic

--- proofing/views.py
+++ proofing/views.py
@@ -16,37 +16,33 @@ def index(request, template_name="proofing/generic_list.html"):
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 def show_category(request, slug, template_name="proofing/generic_list.html"):
-    current = get_object_or_404(Category, slug = slug)
+    object = get_object_or_404(Category, slug = slug)
     
-    objects = Gallery.active.filter(category=current)
+    objects = Gallery.active.filter(category=object)
     
-    breadcrumbtrail = WBBreadcrumbTrail(WBBreadcrumb('Home',reverse('proofing-index')),current)
+    breadcrumbtrail = WBBreadcrumbTrail(WBBreadcrumb('Home',reverse('proofing-index')),object)
     page_title = 'Show Category'
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
 @check_perm_for(Gallery)
-def show_gallery(request, obj, template_name="proofing/generic_list.html"):
-    #current = get_object_or_404(Gallery, slug = gallery_slug)
-    current = obj
-    parent = current.category
+def show_gallery(request, object, template_name="proofing/generic_list.html"):
     
-    objects = Photo.objects.filter(gallery=current)
+    objects = Photo.objects.filter(gallery=object)
     
-    breadcrumbtrail = WBBreadcrumbTrail(WBBreadcrumb('Home',reverse('proofing-index')),parent, current)
+    breadcrumbtrail = WBBreadcrumbTrail(WBBreadcrumb('Home',reverse('proofing-index')),object.category, object)
     page_title = 'Show Gallery'
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
 @check_perm_for(Photo)
-def show_photo(request, obj, template_name="proofing/photo.html"):
-    photo = obj
+def show_photo(request, object, template_name="proofing/photo.html"):
     
     breadcrumbtrail = WBBreadcrumbTrail(
                                         WBBreadcrumb('Home',reverse('proofing-index')),
-                                        photo.gallery.category, 
-                                        photo.gallery,
-                                        photo)
+                                        object.gallery.category, 
+                                        object.gallery,
+                                        object)
     page_title = 'Show Photo'
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
